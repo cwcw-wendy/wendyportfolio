67ec7535eda7f286b9145f859c64b853d98686fb
Yan-Hong-Xi <55658294+Yan-Hong-Xi@users.noreply.github.com>
2023-09-11 01:09:45 +0800

    Modify Design Pages,

--- vite.config.js
+++ vite.config.js
@@ -47,6 +47,6 @@ export default defineConfig({
       ),
     },
     outDir: 'dist',
-    assetsInclude: ['**/*.png', '**/*.jpg', '**/*.svg', '**/*.gif', '**./pdf']
+    assetsInclude: ['**/*.png', '**/*.jpg', '**/*.svg', '**/*.gif']
   },
 });
